fee80e95f31932a1a24356981c79a33524544f28
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-06-29 16:52:30 +0800

    Create application tracking controller

---
+++ Basecode.WebApp/Controllers/ApplicationTrackingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class ApplicationTrackingController : Controller
+    {
+        [Route("applicationtracking")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
