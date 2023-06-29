396eee1772b45265b577f1696fca0f0e83589771
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-06-29 23:33:19 +0800

    Create a SignUpModel

---
+++ Basecode.WebApp/Models/SignUpModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basecode.WebApp.Models
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string? EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Please enter your contact number")]
+        [Display(Name = "Contact Number")]
+        public string? ContactNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address")]
+        [Display(Name = "Address")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your username")]
+        [Display(Name = "Username")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a strong password")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please choose your role")]
+        [Display(Name = "Role")]
+        public string? Role { get; set; }
+    }
+}
