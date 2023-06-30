5aa554bbc9abf3a06c8c7e296ae9be0369dc5602
Estrella Abueva <estrella.abueva@cit.edu>
2023-06-30 08:27:30 +0800

    Add error messages for loginModel

--- Basecode.WebApp/Models/LoginModel.cs
+++ Basecode.WebApp/Models/LoginModel.cs
@@ -1,8 +1,15 @@
-﻿namespace Basecode.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basecode.WebApp.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Please enter your username")]
+        [Display(Name = "Username")]
         public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [Display(Name = "Password")]
         public string? Password { get; set; }
     }
 }
