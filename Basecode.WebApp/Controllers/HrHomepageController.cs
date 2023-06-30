c68ccb856ad81571bcdc6bc158fb5d3592bc1620
thekatisdead <kaherbieto@up.edu.ph>
2023-06-30 10:33:38 +0800

    added comments

--- Basecode.WebApp/Controllers/HrHomepageController.cs
+++ Basecode.WebApp/Controllers/HrHomepageController.cs
@@ -6,6 +6,10 @@ namespace Basecode.WebApp.Controllers
     {
         public IActionResult Index(string Username)
         {
+            // This part is unresolved, mainly because it requires a 'session'
+            // This code just takes the username from the forms and uses it in our dashboard
+            // In the actual program, this will be replaced by the first and last name
+            // once we have defined our models
             ViewBag.Name = Username;
             return View();
         }
