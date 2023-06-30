3785fa61e66c3162bf154696d31bd1eaf7a1e434
Estrella Abueva <estrella.abueva@cit.edu>
2023-06-30 16:01:43 +0800

    Add scripts for DB

---
+++ scripts/dbo.UserManagement.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[UserManagement] (
+    [Id]            INT           NOT NULL,
+    [FirstName]     NVARCHAR (50) NOT NULL,
+    [LastName]      NVARCHAR (50) NOT NULL,
+    [EmailAddress]  NVARCHAR (50) NOT NULL,
+    [ContactNumber] NVARCHAR (50) NOT NULL,
+    [Address]       NVARCHAR (50) NOT NULL,
+    [Username]      NVARCHAR (50) NOT NULL,
+    [Password]      NVARCHAR (50) NOT NULL,
+    [Role]          NVARCHAR (50) NOT NULL,
+	[CreatedTime] DATETIME2 NULL, 
+    [CreatedBy] NVARCHAR(50) NULL, 
+    [UpdatedTime] DATETIME2 NULL, 
+    [UpdatedBy] NVARCHAR(50) NULL
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
