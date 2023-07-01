19176c677f4f6df3cbc264cc104c3dd978a57e0c
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-07-01 20:41:34 +0800

    Create Applicant List Service

---
+++ Basecode.Services/Interfaces/IApplicantListService.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IApplicantListService
+    {
+        /// <summary>
+        /// Retrieves all applicants.
+        /// </summary>
+        /// <returns>A list of Applicant objects representing all applicants.</returns>
+        List<Applicant> RetrieveAll();
+    }
+}
---
+++ Basecode.Services/Services/ApplicantListService.cs
@@ -0,0 +1,31 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Basecode.Data.Repositories;
+using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class ApplicantListService: IApplicantListService
+    {
+        private readonly IApplicantListRepository _repository;
+
+        public ApplicantListService(IApplicantListRepository repository) 
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// Retrieves all applicants.
+        /// </summary>
+        /// <returns>A list of Applicant objects representing all applicants.</returns>
+        public List<Applicant> RetrieveAll()
+        {
+            return _repository.RetrieveAll().ToList();
+        }
+    }
+}
