a4dc7a4addce979b9a386ebcb0191afd0f723356
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-03 12:43:40 +0800

    Add Login Model

---
+++ Basecode.Data/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? ContactNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
