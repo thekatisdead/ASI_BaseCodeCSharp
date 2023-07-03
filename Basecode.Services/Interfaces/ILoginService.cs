60127f2e61e5fe12c5af6ef275856f40a1125fee
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-03 12:44:52 +0800

    Added ILoginService

---
+++ Basecode.Services/Interfaces/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface ILoginService
+    {
+        Login GetByUsername(string username);
+    }
+}
