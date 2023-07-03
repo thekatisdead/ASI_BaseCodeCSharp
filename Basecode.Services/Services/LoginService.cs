dde66536c13e51de6c07a3ca64a79aa79220ea28
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-03 12:45:00 +0800

    Added LoginService

---
+++ Basecode.Services/Services/LoginService.cs
@@ -0,0 +1,26 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly ILoginRepository _loginRepository;
+
+        public LoginService(ILoginRepository loginRepository)
+        {
+            _loginRepository = loginRepository;
+        }
+
+        public Login GetByUsername(string username)
+        {
+            return _loginRepository.GetByUsername(username);
+        }
+    }
+}
