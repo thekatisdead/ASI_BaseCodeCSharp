55b7bf2a3f51c0d05b116edadc6ce09255b4c5eb
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-04 09:30:53 +0800

    Changes in the data type

--- Basecode.Data/Models/Applicant.cs
+++ Basecode.Data/Models/Applicant.cs
@@ -13,11 +13,11 @@ namespace Basecode.Data.Models
     public class Applicant
     {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string EmailAddress { get; set; }
-        public string JobApplied { get; set; }
-        public string Tracker { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? EmailAddress { get; set; }
+        public int JobApplied { get; set; }
+        public string? Tracker { get; set; }
         public DateTime? CreatedTime { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? UpdatedTime { get; set;}
