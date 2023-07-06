b9ca5bbdbc2123261a53503f8331b2c0c5286fd1
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-06 08:46:45 +0800

    Add PublicApplicationFormController

---
+++ Basecode.WebApp/Controllers/PublicApplicationFormController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class PublicApplicationFormController : Controller
+    {
+        [Route("publicapplicationform")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("page2-publicapplicationform")]
+        public IActionResult IndexTwo()
+        {
+            return View();
+        }
+    }
+}
