6710afd26168bf31e44189d8fc220e3893f19667
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-06 22:44:26 +0800

    Implement ApplicantListControllerTests

---
+++ Basecode.Test/Controllers/ApplicantListControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Basecode.Data.ViewModels;
+using Basecode.Services.Interfaces;
+using Basecode.WebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Basecode.Test.Controllers
+{
+    public class ApplicantListControllerTests
+    {
+        private readonly ApplicantListController _controller;
+        private readonly Mock<IApplicantListService> _fakeApplicantListService;
+
+        public ApplicantListControllerTests()
+        {
+            _fakeApplicantListService = new Mock<IApplicantListService>();
+            _controller = new ApplicantListController(_fakeApplicantListService.Object);
+        }
+
+        [Fact]
+        public void Index_ReturnsViewWithModel()
+        {
+            // Arrange
+            var testData = new List<ApplicantListViewModel>
+            {
+                new ApplicantListViewModel { Id = 1, Firstname = "John", Lastname = "Doe", JobApplied = 2 },
+                new ApplicantListViewModel { Id = 2, Firstname = "Jane", Lastname = "Smith", JobApplied = 1 },
+                new ApplicantListViewModel { Id = 3, Firstname = "Bob", Lastname = "Johnson", JobApplied = 3 }
+            };
+
+            _fakeApplicantListService.Setup(s => s.RetrieveAll()).Returns(testData);
+
+            // Act
+            var result = _controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(testData, result.Model);
+            Assert.Null(result.ViewName);
+        }
+    }
+}
