92d5cae2ce514f36b6b515cf56345b226d3b3c1f
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-06 22:57:45 +0800

    Create JobOpeningControllerTests.cs

---
+++ Basecode.Test/Controllers/JobOpeningControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Basecode.Services.Interfaces;
+using Basecode.WebApp.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Test.Controllers
+{
+    public class JobOpeningControllerTests
+    {
+        private readonly JobOpeningController _controller;
+        private readonly Mock<IJobOpeningService> _mockJobOpeningService;
+        public JobOpeningControllerTests()
+        {
+            _mockJobOpeningService = new Mock<IJobOpeningService>();
+            _controller = new JobOpeningController(_mockJobOpeningService.Object);
+        }
+    }
+}
