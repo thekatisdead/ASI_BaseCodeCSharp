0952b907ba14efeaecdd30aa7a1ae2345a8be1fd
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-06 22:56:03 +0800

    Create HrHomepageControllerTests.cs

---
+++ Basecode.Test/Controllers/HrHomepageControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Basecode.WebApp.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Test.Controllers
+{
+    public class HrHomepageControllerTests
+    {
+        private readonly HrHomepageController _controller;
+
+        public HrHomepageControllerTests()
+        {
+            _controller = new HrHomepageController();
+        }
+    }
+}
