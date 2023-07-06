ee2432e8f753cb2dba9d77f10c4c1f204cc436cf
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-06 19:16:49 +0800

    Update PublicApplicationFormController

--- Basecode.WebApp/Controllers/PublicApplicationFormController.cs
+++ Basecode.WebApp/Controllers/PublicApplicationFormController.cs
@@ -4,19 +4,19 @@ namespace Basecode.WebApp.Controllers
 {
     public class PublicApplicationFormController : Controller
     {
-        [Route("publicapplicationform")]
+        [Route("public-application-form")]
         public IActionResult Index()
         {
             return View();
         }
 
-        [Route("page2-publicapplicationform")]
+        [Route("page2-public-application-form")]
         public IActionResult IndexTwo()
         {
             return View();
         }
 
-        [Route("page3-publicapplicationform")]
+        [Route("page3-public-application-form")]
         public IActionResult IndexThree()
         {
             return View();
