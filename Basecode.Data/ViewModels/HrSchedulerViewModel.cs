9370c734ae714a222681ebbf541e08028ce9a226
itsjustjl <jhonlloyd.cabarrubias@cit.edu>
2023-07-07 15:13:15 +0800

    Create HrScheduler view model

---
+++ Basecode.Data/ViewModels/HrSchedulerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.ViewModels
+{
+    public class HrSchedulerViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? AdditionalInstruction { get; set; }
+    }
+}
