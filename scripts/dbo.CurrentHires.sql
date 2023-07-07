d8c57040db094a6ecb252aa2ebf1e939c152f1b8
thekatisdead <kaherbieto@up.edu.ph>
2023-07-07 17:18:05 +0800

    CurrentHires Created

---
+++ scripts/dbo.CurrentHires.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CurrentHires] (
+    [Id]          INT Identity (1,1) NOT NULL,
+    [ApplicantID] INT           NULL,
+    [JobID]       INT           NULL,
+    [CreatedTime] DATETIME      NULL,
+    [CreatedBy]   NVARCHAR (50) NULL,
+    [UpdatedTime] DATETIME      NULL,
+    [UpdatedBy]   NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
