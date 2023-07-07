7c9d1a2ee8a6475163a61449fdadd4903919bbc2
itsjustjl <jhonlloyd.cabarrubias@cit.edu>
2023-07-07 19:02:19 +0800

    Add validation for HR Scheduler module

--- Basecode.Data/ViewModels/HrSchedulerViewModel.cs
+++ Basecode.Data/ViewModels/HrSchedulerViewModel.cs
@@ -9,22 +9,32 @@ namespace Basecode.Data.ViewModels
 {
     public class HrSchedulerViewModel
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(15, ErrorMessage = "First name must not exceed 15 characters.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(15, ErrorMessage = "Last name must not exceed 15 characters.")]
         public string LastName { get; set; }
 
-        [EmailAddress]
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string EmailAddress { get; set; }
 
+        [Required(ErrorMessage = "Date is required.")]
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
+        [Required(ErrorMessage = "Start time is required.")]
         [DataType(DataType.Time)]
         public DateTime StartTime { get; set; }
 
+        [Required(ErrorMessage = "End time is required.")]
         [DataType(DataType.Time)]
+        [Compare("StartTime", ErrorMessage = "Start time must be earlier than End time.")]
         public DateTime EndTime { get; set; }
 
+        [StringLength(200, ErrorMessage = "Additional instruction must not exceed 200 characters.")]
         public string? AdditionalInstruction { get; set; }
     }
 }
