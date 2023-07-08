79b01f30c57c64651d6eed685a17c3c8820586cc
jimwill <jimwillcapino@gmail.com>
2023-07-08 23:08:28 +0800

    Added a job listing function on the admin side with pagination.

--- Basecode.WebApp/Controllers/AdminController.cs
+++ Basecode.WebApp/Controllers/AdminController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Basecode.Services.Interfaces;
+using Basecode.Data.Models;
 namespace Basecode.WebApp.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IJobOpeningService _jobOpeningService;
+
+        public AdminController(IJobOpeningService jobOpeningService)
+        {
+            _jobOpeningService= jobOpeningService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult AdminJobListing()
+        {
+            var job = _jobOpeningService.RetrieveAll();
+            return View(job);
         }
     }
 }
