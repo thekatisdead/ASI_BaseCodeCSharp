d671c818d4207df4d71ab1e907f00a05d355453d
jimwill <jimwillcapino@gmail.com>
2023-07-08 16:17:59 +0800

    Added side and upper navigation bar

---
+++ Basecode.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
