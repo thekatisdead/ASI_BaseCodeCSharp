63c6bcba3864870a272df8f9234b4f04f8d66b90
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-10 15:52:34 +0800

    Trying to fix PublicApplicationFormController

--- Basecode.WebApp/Controllers/PublicApplicationFormController.cs
+++ Basecode.WebApp/Controllers/PublicApplicationFormController.cs
@@ -1,52 +1,75 @@
 ﻿using Basecode.Services.Interfaces;
 using Basecode.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Basecode.Data.Models;
 
 namespace Basecode.WebApp.Controllers
 {
     public class PublicApplicationFormController : Controller
     {
         private readonly IPublicApplicationFormService _service;
+        private PublicApplicationFormViewModel? _viewModel; // Member variable to store the view model
 
         public PublicApplicationFormController(IPublicApplicationFormService service)
         {
             _service = service;
+            _viewModel = new PublicApplicationFormViewModel();
         }
 
         [Route("public-application-form")]
         public IActionResult Index()
         {
-            return View();
+            return View(_viewModel);
         }
 
         [HttpPost]
         [Route("page2-public-application-form")]
         public IActionResult IndexTwo(PublicApplicationFormViewModel viewModel)
         {
-            // Validate the submitted data if necessary
+            // Update the view model with the submitted data
+            _viewModel.FirstName = viewModel.FirstName;
+            _viewModel.LastName = viewModel.LastName;
+            _viewModel.PhoneNumber = viewModel.PhoneNumber;
+            _viewModel.EmailAddress = viewModel.EmailAddress;
+            _viewModel.Address = viewModel.Address;
 
-            // Pass the data to the next view
-            return View("IndexTwo", viewModel);
+            // Continue to the next view
+            return View("IndexTwo", _viewModel);
         }
 
         [HttpPost]
         [Route("page3-public-application-form")]
         public IActionResult IndexThree(PublicApplicationFormViewModel viewModel)
         {
-            // Validate the submitted data if necessary
+            // Update the view model with the submitted data
+            _viewModel.Time = viewModel.Time;
+            _viewModel.PositionType = viewModel.PositionType;
+            _viewModel.EmploymentType = viewModel.EmploymentType;
 
-            // Call the service method to create the form
-            //_service.AddForm(viewModel);
-
-            // Redirect or show a success message to the user
-            return View("IndexThree", viewModel);
+            // Continue to the next view
+            return View("IndexThree", _viewModel);
         }
 
         [HttpPost]
         public IActionResult AddForm(PublicApplicationFormViewModel viewModel)
         {
-            _service.AddForm(viewModel);
-            //TempData["SuccessMessage"] = "Successfully created an account! You may proceed to log in to our system.";
+            // Update the view model with the submitted data
+            _viewModel.Achievements = viewModel.Achievements;
+            _viewModel.ReferenceOneFullName = viewModel.ReferenceOneFullName;
+            _viewModel.RelationshipOne = viewModel.RelationshipOne;
+            _viewModel.ContactInfoOne = viewModel.ContactInfoOne;
+            _viewModel.ReferenceTwoFullName = viewModel.ReferenceTwoFullName;
+            _viewModel.RelationshipTwo = viewModel.RelationshipTwo;
+            _viewModel.ContactInfoTwo = viewModel.ContactInfoTwo;
+            _viewModel.ReferenceThreeFullName = viewModel.ReferenceThreeFullName;
+            _viewModel.RelationshipThree = viewModel.RelationshipThree;
+            _viewModel.ContactInfoThree = viewModel.ContactInfoThree;
+            _viewModel.CurriculumVitae = viewModel.CurriculumVitae;
+
+            // Call the service method to create the form
+            _service.AddForm(_viewModel);
+
+            // Redirect or show a success message to the user
             return RedirectToAction("Index");
         }
     }
