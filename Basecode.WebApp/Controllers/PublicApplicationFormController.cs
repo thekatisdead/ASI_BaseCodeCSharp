b0fe3a2cd37a12eed8c8e023b3f8a2848ee2b240
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-10 15:46:05 +0800

    Edit PublicApplicationFormController

--- Basecode.WebApp/Controllers/PublicApplicationFormController.cs
+++ Basecode.WebApp/Controllers/PublicApplicationFormController.cs
@@ -20,29 +20,6 @@ namespace Basecode.WebApp.Controllers
         }
 
         [HttpPost]
-        [Route("page2-public-application-form")]
-        public IActionResult IndexTwo(PublicApplicationFormViewModel viewModel)
-        {
-            // Validate the submitted data if necessary
-
-            // Pass the data to the next view
-            return View("IndexTwo", viewModel);
-        }
-
-        [HttpPost]
-        [Route("page3-public-application-form")]
-        public IActionResult IndexThree(PublicApplicationFormViewModel viewModel)
-        {
-            // Validate the submitted data if necessary
-
-            // Call the service method to create the form
-            //_service.AddForm(viewModel);
-
-            // Redirect or show a success message to the user
-            return View("IndexThree", viewModel);
-        }
-
-        [HttpPost]
         public IActionResult AddForm(PublicApplicationFormViewModel viewModel)
         {
             _service.AddForm(viewModel);
