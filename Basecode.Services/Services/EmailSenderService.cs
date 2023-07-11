c5f5f951579ff93ded397353f92f4ef9729003f0
itsjustjl <jhonlloyd.cabarrubias@cit.edu>
2023-07-11 10:59:35 +0800

    Create send email function

---
+++ Basecode.Services/Services/EmailSenderService.cs
@@ -0,0 +1,34 @@
+﻿using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class EmailSenderService : IEmailSenderService
+    {
+        public Task SendEmailAsync(string receiverEmail, string subject, string message)
+        {
+            // change the authentication credentials to test if it works in your device
+            string userEmail = "outlook-email@.com";
+            string password = "password";
+
+            var client = new SmtpClient("smtp.office365.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(userEmail, password)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: userEmail,
+                                to: receiverEmail,
+                                subject,
+                                message));
+        }
+    }
+}
