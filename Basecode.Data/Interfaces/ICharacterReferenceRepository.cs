0f30ce2915e15f657a61455be2f3e9c64fc85ff0
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-07-11 16:52:21 +0800

    Create documentation for character reference repository

--- Basecode.Data/Interfaces/ICharacterReferenceRepository.cs
+++ Basecode.Data/Interfaces/ICharacterReferenceRepository.cs
@@ -9,8 +9,23 @@ namespace Basecode.Data.Interfaces
 {
     public interface ICharacterReferenceRepository
     {
+        /// <summary>
+        /// Retrieves all character references.
+        /// </summary>
+        /// <returns>An IQueryable of CharacterReference.</returns>
         IQueryable<CharacterReference> RetrieveAll();
+
+        /// <summary>
+        /// Retrieves a character reference by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the character reference.</param>
+        /// <returns>The CharacterReference with the specified ID.</returns>
         CharacterReference GetById(int id);
+
+        /// <summary>
+        /// Adds a new character reference.
+        /// </summary>
+        /// <param name="characterReference">The character reference to add.</param>
         void Add(CharacterReference characterReference);
     }
 }
