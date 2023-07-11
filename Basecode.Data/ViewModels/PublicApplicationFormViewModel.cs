53720131f8e60f75538ff213901c213fd7ec5128
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-11 11:42:44 +0800

    Add Validations in Public Application Form View Model

--- Basecode.Data/ViewModels/PublicApplicationFormViewModel.cs
+++ Basecode.Data/ViewModels/PublicApplicationFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,26 +10,70 @@ namespace Basecode.Data.ViewModels
     public class PublicApplicationFormViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Contact number must be 11 digits.")]
         public string? PhoneNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email format.")]
         public string? EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? Address { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? Time { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? PositionType { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? EmploymentType { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? School { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? SchoolDepartment { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? Achievements { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ReferenceOneFullName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? RelationshipOne { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ContactInfoOne { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ReferenceTwoFullName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? RelationshipTwo { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ContactInfoTwo { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ReferenceThreeFullName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? RelationshipThree { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? ContactInfoThree { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
         public string? CurriculumVitae { get; set; }
         public DateTime? CreatedTime { get; set; } = DateTime.Now;
         public string? CreatedBy { get; set; } = System.Environment.UserName;
