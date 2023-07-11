2a46ed91f2a960471d6eb95607cd86d2d24ad956
itsjustjl <jhonlloyd.cabarrubias@cit.edu>
2023-07-11 11:02:02 +0800

    Configure HrScheduler controller for initial testing

--- Basecode.WebApp/Controllers/HrSchedulerController.cs
+++ Basecode.WebApp/Controllers/HrSchedulerController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basecode.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Basecode.WebApp.Controllers
 {
     public class HrSchedulerController : Controller
     {
-        public IActionResult Index()
+        private readonly IEmailSenderService _emailSender;
+
+        public HrSchedulerController(IEmailSenderService emailSender)
         {
+            _emailSender = emailSender;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            // change the receiver email to test in your device
+            var receiverEmail = "receiver-email@.com";
+            var subject = "test";
+            var message = "testing testing email email";
+
+            await _emailSender.SendEmailAsync(receiverEmail, subject, message);
             return View();
         }
     }
