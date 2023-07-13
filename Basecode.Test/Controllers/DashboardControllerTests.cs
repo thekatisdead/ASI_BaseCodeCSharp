a5d459e13666f4762b82fe3f08459f6d328e3f8d
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-13 09:42:56 +0800

    Add DashboardControllerTests

--- Basecode.Test/Controllers/DashboardControllerTests.cs
+++ Basecode.Test/Controllers/DashboardControllerTests.cs
@@ -1,7 +1,27 @@
-﻿namespace Basecode.Test.Controllers
+﻿using Basecode.WebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.Test.Controllers
 {
     public class DashboardControllerTests
     {
+        private readonly DashboardController _controller;
 
+        public DashboardControllerTests()
+        {
+            _controller = new DashboardController();
+        }
+
+        [Fact]
+        public void Index_ReturnsView()
+        {
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+        }
     }
 }
