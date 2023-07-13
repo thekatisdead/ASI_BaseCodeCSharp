f8cd91f3bd350c7e665d705135de7a6a02eecec8
itsjustjl <jhonlloyd.cabarrubias@cit.edu>
2023-07-13 12:10:56 +0800

    Add controller Schedule action method

--- Basecode.WebApp/Controllers/HrSchedulerController.cs
+++ Basecode.WebApp/Controllers/HrSchedulerController.cs
@@ -1,4 +1,5 @@
-﻿using Basecode.Services.Interfaces;
+﻿using Basecode.Data.Models;
+using Basecode.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basecode.WebApp.Controllers
@@ -12,24 +13,15 @@ namespace Basecode.WebApp.Controllers
             _emailSender = emailSender;
         }
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            // change the receiver email to test in your device
-            var receiverEmail = "receiver-email@gmail.com";
-            var companyName = "Alliance";
-            var jobPosition = "Bottom";
-
-            //await _emailSender.SendEmailAsync(receiverEmail, subject, message);
-            //await _emailSender.SendEmailAsyncWithHtml(receiverEmail, subject);
-            //_emailSender.SendEmailRejectApplication(receiverEmail, "Mr Kat" ,companyName, jobPosition);
-            _emailSender.SendEmailHRApplicationDecision(receiverEmail, 1,"Mr Kat" , jobPosition);
-            //_emailSender.SendEmailOnUpdateApplicantStatus(receiverEmail, "Mr Kat", "Bottom", "Top");
             return View();
+        }
+
+        public IActionResult Schedule(HrScheduler HrScheduler)
+        {
+            _emailSender.SendEmailInterviewSchedule(HrScheduler);
+            return RedirectToAction("Index");
         }
     }
 }
