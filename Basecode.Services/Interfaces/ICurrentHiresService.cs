363a46bbcfaca10bd16770fef38b203f18f15da1
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-17 13:17:49 +0800

    Add ICurrentHiresService

---
+++ Basecode.Services/Interfaces/ICurrentHiresService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface ICurrentHiresService
+    {
+        /// <summary>
+        /// Retrieves all applicants from the database.
+        /// </summary>
+        /// <returns>An IQueryable of Applicant containing all applicants.</returns>
+        void AddHire(int applicantId, int jobId);
+    }
+}
