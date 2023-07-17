5d73478c744bcf681bee9a16c192418d43900d0a
Estrella Abueva <estrella.abueva@cit.edu>
2023-07-17 13:17:59 +0800

    Add CurrentHiresService

---
+++ Basecode.Services/Services/CurrentHiresService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Basecode.Data.Interfaces;
+using Basecode.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Services
+{
+    public class CurrentHiresService : ICurrentHiresService
+    {
+        private readonly ICurrentHiresRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CurrentHiresService(ICurrentHiresRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void AddHire(int applicantId, int jobId)
+        {
+            _repository.AddHire(applicantId, jobId);
+        }
+    }
+}
