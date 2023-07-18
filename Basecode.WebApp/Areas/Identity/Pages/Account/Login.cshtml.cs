43a288860919f8787381daead1b570ffd03828f7
John Gabriel B. Rago <johngabriel.rago@cit.edu>
2023-07-18 15:35:16 +0800

    Make Login and Register Authentication

--- Basecode.WebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Basecode.WebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,7 +66,7 @@ namespace Basecode.WebApp.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [EmailAddress]
-            public string Email { get; set; }
+            public string Username { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -83,6 +83,9 @@ namespace Basecode.WebApp.Areas.Identity.Pages.Account
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
+
+        //public string? Username { get; set; }
+        //public string? Password { get; set; }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -111,7 +114,7 @@ namespace Basecode.WebApp.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
