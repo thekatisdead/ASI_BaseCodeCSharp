6d94415e640df4a7d9beda9f5880dc968342dfb2
jimwill <jimwillcapino@gmail.com>
2023-07-19 16:51:20 +0800

    Added a Repository and service for Interviewer.

---
+++ Basecode.Data/Repositories/InterviewerRepository.cs
@@ -0,0 +1,24 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Repositories
+{
+    public class InterviewerRepository: BaseRepository, IInterviewerRepository
+    {
+        private readonly BasecodeContext _context;
+        public InterviewerRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork) 
+        {
+            _context= context;
+        }
+        public void Add(Interviewer interviewer)
+        {
+            _context.Interviewer.Add(interviewer);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Basecode.Services/Services/InterviewerServices.cs
@@ -0,0 +1,26 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Services.Interfaces;
+namespace Basecode.Services.Services
+{
+    public class InterviewerServices: IInterviewerServices
+    {
+        private readonly IInterviewerRepository _interviewerRepository;
+
+        public InterviewerServices(IInterviewerRepository interviewerRepository)
+        {
+            _interviewerRepository = interviewerRepository;
+        }
+
+        public void Add(Interviewer interviewer)
+        {
+            _interviewerRepository.Add(interviewer);
+        }
+    }
+}
