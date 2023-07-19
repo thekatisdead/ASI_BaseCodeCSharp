52bce403f5398ab86b2d22fd7088b05512bb086b
jimwill <jimwillcapino@gmail.com>
2023-07-19 16:10:55 +0800

    Created a interview UI to manage HR scheduling as well as the Interview model.

--- Basecode.WebApp/Controllers/HrSchedulerController.cs
+++ Basecode.WebApp/Controllers/HrSchedulerController.cs
@@ -6,9 +6,13 @@ namespace Basecode.WebApp.Controllers
     public class HrSchedulerController : Controller
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
-        public IActionResult Index()
+        public IActionResult AddInterviewer()
         {
             _logger.Trace("HrScheduler Controller Accessed");
+            return View();
+        }
+        public IActionResult home()
+        {
             return View();
         }
     }
