8a3b779c411dfeb6d24e6027ac78dd80e0017421
jimwill <jimwillcapino@gmail.com>
2023-07-20 14:51:53 +0800

    Created AddSchedule View and the Schedule model to represent the Schedule table.

---
+++ Basecode.Data/Models/Schedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; } 
+        public int InterviewerId { get; set; } 
+        public int JobId { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public DateTime Date { get; set; }
+        public string Instruction { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = System.Environment.UserName;
+        public DateTime UpdatedTime { get; set; }
+        public string UpdatedBy { get; set; } = System.Environment.UserName;
+    }
+}
