5250f946625ab0b56a2cbac86ea9f9636ad572e3
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-07-26 12:36:58 +0800

    Create test for new method

--- Basecode.Test/Controllers/CharacterReferenceControllerTests.cs
+++ Basecode.Test/Controllers/CharacterReferenceControllerTests.cs
@@ -127,7 +127,99 @@ namespace Basecode.Test.Controllers
             // Assert
             Assert.NotNull(result);
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
-            Assert.Equal("An error occurred while generating the report.", badRequestResult.Value);
+            Assert.Equal("An error occurred while generating the character reference respondents list.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void GenerateCharacterReferenceReport_ValidRespondentId_ReturnsViewWithData()
+        {
+            // Arrange
+            int respondentId = 1;
+            var testData = new List<CharacterReferenceViewModel>
+            {
+                new CharacterReferenceViewModel
+                {
+                    Id = 1,
+                    CandidateFirstName = "John",
+                    CandidateLastName = "Doe",
+                    Position = "Software Developer",
+                    RelationshipDuration = "1 year",
+                    Relationship = "Friend",
+                    CharacterEthics = "Good",
+                    Qualifications = "Good",
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    JobTitle = "Software Developer",
+                    WorkedWithCandidate = true,
+                    ReasonToHire = "Good"
+                },
+                // Add more related respondents if needed
+            };
+
+            _mockCharacterReferenceService.Setup(s => s.RetrieveResponses()).Returns(testData);
+
+            // Act
+            var result = _controller.GenerateCharacterReferenceReport(respondentId);
+
+            // Assert
+            Assert.NotNull(result);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsAssignableFrom<List<CharacterReferenceViewModel>>(viewResult.Model);
+            var model = (List<CharacterReferenceViewModel>)viewResult.Model;
+            Assert.Equal(testData, model);
+            Assert.Equal(respondentId, viewResult.ViewData["CurrentRespondentId"]);
+        }
+
+        [Fact]
+        public void GenerateCharacterReferenceReport_InvalidRespondentId_ReturnsNotFound()
+        {
+            // Arrange
+            int respondentId = 100;
+            var testData = new List<CharacterReferenceViewModel>
+            {
+                new CharacterReferenceViewModel
+                {
+                    Id = 1,
+                    CandidateFirstName = "John",
+                    CandidateLastName = "Doe",
+                    Position = "Software Developer",
+                    RelationshipDuration = "1 year",
+                    Relationship = "Friend",
+                    CharacterEthics = "Good",
+                    Qualifications = "Good",
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    JobTitle = "Software Developer",
+                    WorkedWithCandidate = true,
+                    ReasonToHire = "Good"
+                },
+                // Add more related respondents if needed
+            };
+
+            _mockCharacterReferenceService.Setup(s => s.RetrieveResponses()).Returns(testData);
+
+            // Act
+            var result = _controller.GenerateCharacterReferenceReport(respondentId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void GenerateCharacterReferenceReport_Exception_ReturnsBadRequest()
+        {
+            // Arrange
+            int respondentId = 1;
+            _mockCharacterReferenceService.Setup(s => s.RetrieveResponses()).Throws(new Exception("Test exception"));
+
+            // Act
+            var result = _controller.GenerateCharacterReferenceReport(respondentId);
+
+            // Assert
+            Assert.NotNull(result);
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("An error occurred while generating the character reference report.", badRequestResult.Value);
         }
     }
 }
