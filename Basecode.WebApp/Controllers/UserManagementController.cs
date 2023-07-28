d13ee0d7d3c88caa4ce6322bd0f1e0e5eafbf4b1
Jie Ann Leonor <jieann.leonor@cit.edu>
2023-07-28 16:26:20 +0800

    Add user management controller

---
+++ Basecode.WebApp/Controllers/UserManagementController.cs
@@ -0,0 +1,28 @@
+﻿using Basecode.Services.Interfaces;
+using Basecode.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        private readonly IUserViewService _service;
+
+        public UserManagementController(IUserViewService userService)
+        {
+            _service = userService;
+        }
+
+        public IActionResult UpdateUser(int id)
+        {
+            var data = _service.GetUserById(id);
+            return View(data);
+        }
+
+        public IActionResult DeleteUser(int id)
+        {
+            var data = _service.GetUserById(id);
+            return View(data);
+        }
+    }
+}
