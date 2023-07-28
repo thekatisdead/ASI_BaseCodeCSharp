207ba5bd17c3967d16831cc282f9e310610c7f45
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-28 14:37:04 +0800

    Implement Loggings for UserViewRepository

--- Basecode.Data/Repositories/UserViewRepository.cs
+++ Basecode.Data/Repositories/UserViewRepository.cs
@@ -6,19 +6,32 @@ using System.Threading.Tasks;
 using Basecode.Data.ViewModels;
 using Basecode.Data.Interfaces;
 using Basecode.Data.Models;
+using NLog;
 
 namespace Basecode.Data.Repositories
 {
-    public class UserViewRepository: BaseRepository, IUserViewRepository
+    public class UserViewRepository : BaseRepository, IUserViewRepository
     {
         private BasecodeContext _context;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         public UserViewRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
         {
             _context = context;
         }
+
         public IQueryable<SignUp> RetrieveAll()
         {
-            return this.GetDbSet<SignUp>();
+            try
+            {
+                _logger.Info("Retrieving all users from the database.");
+                return this.GetDbSet<SignUp>();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error occurred while retrieving all user views: {errorMessage}", ex.Message);
+                throw;
+            }
         }
     }
 }
