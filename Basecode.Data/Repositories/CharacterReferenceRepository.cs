5763bc750e4fc29c0b1108bcc67bad2a93dcd779
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-07-28 14:35:37 +0800

    Implement Loggings for CharacterReferenceRepository

--- Basecode.Data/Repositories/CharacterReferenceRepository.cs
+++ Basecode.Data/Repositories/CharacterReferenceRepository.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Basecode.Data.Repositories
 {
-    public class CharacterReferenceRepository: BaseRepository, ICharacterReferenceRepository
+    public class CharacterReferenceRepository : BaseRepository, ICharacterReferenceRepository
     {
-        public readonly BasecodeContext _context;
+        private readonly BasecodeContext _context;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
         public CharacterReferenceRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
         {
@@ -23,7 +25,16 @@ namespace Basecode.Data.Repositories
         /// <returns>An IQueryable of CharacterReference.</returns>
         public IQueryable<CharacterReference> RetrieveAll()
         {
-            return this.GetDbSet<CharacterReference>();
+            try
+            {
+                _logger.Info("Retrieving all character references from the database.");
+                return this.GetDbSet<CharacterReference>();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error occurred while retrieving all character references: {errorMessage}", ex.Message);
+                throw;
+            }
         }
 
         /// <summary>
@@ -32,8 +43,17 @@ namespace Basecode.Data.Repositories
         /// <param name="characterReference">The character reference to add.</param>
         public void Add(CharacterReference characterReference)
         {
-            _context.CharacterReference.Add(characterReference);
-            _context.SaveChanges();
+            try
+            {
+                _context.CharacterReference.Add(characterReference);
+                _context.SaveChanges();
+                _logger.Info("Character reference with ID {characterReferenceId} added successfully.", characterReference.Id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Error occurred while adding a new character reference: {errorMessage}", ex.Message);
+                throw;
+            }
         }
     }
 }
